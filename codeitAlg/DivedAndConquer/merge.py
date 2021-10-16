cd1fa7ef445ae4710d1e1ef14b388fc18ea05df9
lopahn2 <lopahn2@gmail.com>
2021-10-16 14:36:36 +0000

    2021-10-16

--- codeitAlg/DivedAndConquer/merge.py
+++ codeitAlg/DivedAndConquer/merge.py
@@ -24,11 +24,21 @@ def merge(list1, list2):
 
     return merged_list
         
+# 합병 정렬
+def merge_sort(my_list):
+    if len(my_list) == 2:
+        return merge(my_list[:1], my_list[1:])
+    elif len(my_list) == 1:
+        return my_list
+  
     
+    midIdx = len(my_list) // 2
+    list1 = my_list[:midIdx]
+    list2 = my_list[midIdx:]
+    return merge(merge_sort(list1), merge_sort(list2))
+    
+
 # 테스트
-print(merge([1],[]))
-print(merge([],[1]))
-print(merge([2],[1]))
-print(merge([1, 2, 3, 4],[5, 6, 7, 8]))
-print(merge([5, 6, 7, 8],[1, 2, 3, 4]))
-print(merge([4, 7, 8, 9],[1, 3, 6, 10]))
+print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
+print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
+print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))
