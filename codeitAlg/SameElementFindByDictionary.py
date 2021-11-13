18422d6e78b32eaff33f4fe947f97b0a5bf60fe9
lopahn2 <lopahn2@gmail.com>
2021-11-13 10:55:33 +0000

    2021-11-13

---
+++ codeitAlg/SameElementFindByDictionary.py
@@ -0,0 +1,16 @@
+
+def find_same_number(some_list):
+    # 이미 나온 요소를 저장시켜줄 사전
+    elements_seen_so_far = {}
+
+    for element in some_list:
+        # 이미 나온 요소인지 확인하고 맞으면 요소를 리턴한다
+        if element in elements_seen_so_far:
+            return element
+
+        # 해당 요소를 사전에 저장시킨다
+        elements_seen_so_far[element] = True
+# 중복되는 수 ‘하나’만 리턴합니다.
+print(find_same_number([1, 4, 3, 5, 3, 2]))
+print(find_same_number([4, 1, 5, 2, 3, 5]))
+print(find_same_number([5, 2, 3, 4, 1, 6, 7, 8, 9, 3]))
