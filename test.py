9b07012a27c874d2415fe92307569cc30f0d8309
lopahn2 <lopahn2@gmail.com>
2022-06-01 14:32:14 +0000

    22-06-01

--- test.py
+++ test.py
@@ -1,32 +1,9 @@
 import sys
-queue = []
-
 N = int(sys.stdin.readline())
-for _ in range(N):
-    command = sys.stdin.readline().split()
-    if command[0] == 'push':
-        queue.append(int(command[1]))
-    elif command[0] == 'pop':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            temp = queue[0]
-            del queue[0]
-            print(temp)
-    elif command[0] == 'size':
-        print(len(queue))
-    elif command[0] == 'empty':
-        if len(queue) == 0:
-            print(1)
-        else:
-            print(0)
-    elif command[0] == 'front':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue[0])
-    elif command[0] == 'back':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue[-1])
+unsortedList = []
+for i in range(N):
+    unsortedList.append(int(sys.stdin.readline()))
+
+for num in sorted(unsortedList):
+    print(num)
+
