5c84690cf330062a71e8e2fccae5f3b08d115219
lopahn2 <lopahn2@gmail.com>
2022-06-05 14:39:01 +0000

    2022-06-05

--- test.py
+++ test.py
@@ -1,9 +1,30 @@
 import sys
-N = int(sys.stdin.readline())
-unsortedList = []
-for i in range(N):
-    unsortedList.append(int(sys.stdin.readline()))
+n = int(sys.stdin.readline())
 
-for num in sorted(unsortedList):
-    print(num)
+colorPaper = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
+white = 0
+blue = 0
+
+def divideAndConquer(x, y, n):
+    global white, blue
+    sameColor = colorPaper[y][x]
+    for i in range(y, y+n):
+        for j in range(x, x+n):
+            if sameColor != colorPaper[i][j]:
+                divideAndConquer(x,y,n//2)
+                divideAndConquer(x+n//2,y,n//2)
+                divideAndConquer(x,y+n//2,n//2)
+                divideAndConquer(x+n//2,y+n//2,n//2)
+                return
+    if sameColor == 0 :
+        white += 1
+        return
+    else:
+        blue += 1
+        return
+    
+divideAndConquer(0,0,n)
+print(white)
+print(blue)
+                
