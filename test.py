c8ac67f9d5c04b5b0cd8b21bdae5f9cbdea8e60f
lopahn2 <lopahn2@gmail.com>
2022-06-20 14:39:35 +0000

    11

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print(1 ^ 1)
+N = int(input())
+members = []
+for i in range(N):
+    x, y = map(int, input().split())
+    members.append((x, y))
+tier = [1 for i in range(N)]
+for i in range(N):
+    for j in range(N):
+        if j == i :
+            continue
+        if members[i][0] < members[j][0] and members[i][1] < members[j][1]:
+            tier[i] += 1
+print(' '.join(map(str,tier)))
