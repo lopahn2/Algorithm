7c9dd10972cce261d0843a4938aa83b56f70d5ee
lopahn2 <lopahn2@gmail.com>
2022-06-21 14:46:17 +0000

    1

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
-print("hello")
+from collections import deque
+
+while True:
+    s = input()
+    if s == ".":
+        break
+    queue = deque()
+    temp = True
+    for ss in s:
+        if ss == "(" or ss == "[":
+            queue.append(ss)
+        elif ss == ")":
+            if len(queue) == 0 or queue[-1] == "[":
+                temp = False
+                break
+            elif queue[-1] == "(":
+                queue.pop()
+        elif ss == "]":
+            if len(queue) == 0 or queue[-1] == "(":
+                temp = False
+                break
+            elif queue[-1] == "[":
+                queue.pop()
+
+    if len(queue) == 0 and temp:
+        print("yes")
+    else:
+        print("no")
+        
+
