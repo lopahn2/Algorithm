bce1141fe4773ddb86e1d07f525dfff0d03c8d7a
lopahn2 <lopahn2@gmail.com>
2022-06-22 14:15:24 +0000

    1

--- test.py
+++ test.py
@@ -1,30 +1,40 @@
-from collections import deque
-
-while True:
-    s = input()
-    if s == ".":
-        break
-    queue = deque()
-    temp = True
-    for ss in s:
-        if ss == "(" or ss == "[":
-            queue.append(ss)
-        elif ss == ")":
-            if len(queue) == 0 or queue[-1] == "[":
-                temp = False
-                break
-            elif queue[-1] == "(":
-                queue.pop()
-        elif ss == "]":
-            if len(queue) == 0 or queue[-1] == "(":
-                temp = False
-                break
-            elif queue[-1] == "[":
-                queue.pop()
-
-    if len(queue) == 0 and temp:
-        print("yes")
-    else:
-        print("no")
+def solution(id_list, report, k):
+    answer = []
+    reportMember = {}
+    reportedMember = {}
+    reportLog = []
+    for member in id_list:
+        reportMember[member] = 0
+        reportedMember[member] = 0
         
+    for reportInfo in report:
+        reporter, reportee = reportInfo.split()
+        reportLog.append((reporter, reportee))
+    reportLog = list(dict.fromkeys(reportLog))
+    
+    for rep in reportLog:
+        reportedMember[rep[1]] += 1
+        if reportedMember[rep[1]] % k == 0:
+            for repInfo in reportLog:
+                if repInfo[1] == rep[1]:
+                    reportMember[repInfo[0]] += 1
+    
+    for reporter in reportMember.keys():
+        answer.append(reportMember[reporter])
 
+        
+    return answer
+
+
+def solution(id_list, report, k):
+    reported_cnt = {x:0 for x in id_list}
+    received_mail_cnt = [0] * len(id_list)
+    
+    for r in set(report):  # report 형식 : '이용자id 신고한id'
+        reported_cnt[r.split()[1]] += 1
+            
+    for r in set(report):
+        if reported_cnt[r.split()[1]] >= k:
+            received_mail_cnt[id_list.index(r.split()[0])] += 1
+    
+    return received_mail_cnt
