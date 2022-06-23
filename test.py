374b36ce256477fb2bee477b66a2a8583663a0c1
lopahn2 <lopahn2@gmail.com>
2022-06-23 10:53:39 +0000

    1

--- test.py
+++ test.py
@@ -1,40 +1,28 @@
-def solution(id_list, report, k):
-    answer = []
-    reportMember = {}
-    reportedMember = {}
-    reportLog = []
-    for member in id_list:
-        reportMember[member] = 0
-        reportedMember[member] = 0
-        
-    for reportInfo in report:
-        reporter, reportee = reportInfo.split()
-        reportLog.append((reporter, reportee))
-    reportLog = list(dict.fromkeys(reportLog))
-    
-    for rep in reportLog:
-        reportedMember[rep[1]] += 1
-        if reportedMember[rep[1]] % k == 0:
-            for repInfo in reportLog:
-                if repInfo[1] == rep[1]:
-                    reportMember[repInfo[0]] += 1
-    
-    for reporter in reportMember.keys():
-        answer.append(reportMember[reporter])
-
-        
+def solution(new_id):
+    answer = ''
+    # 1
+    new_id = new_id.lower()
+    # 2
+    for c in new_id:
+        if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
+            answer += c
+    # 3
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    # 4
+    if answer[0] == '.':
+        answer = answer[1:] if len(answer) > 1 else '.'
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    # 5
+    if answer == '':
+        answer = 'a'
+    # 6
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    # 7
+    while len(answer) < 3:
+        answer += answer[-1]
     return answer
-
-
-def solution(id_list, report, k):
-    reported_cnt = {x:0 for x in id_list}
-    received_mail_cnt = [0] * len(id_list)
-    
-    for r in set(report):  # report 형식 : '이용자id 신고한id'
-        reported_cnt[r.split()[1]] += 1
-            
-    for r in set(report):
-        if reported_cnt[r.split()[1]] >= k:
-            received_mail_cnt[id_list.index(r.split()[0])] += 1
-    
-    return received_mail_cnt
