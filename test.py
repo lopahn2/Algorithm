abe3a68c89f41f474ef2b64cbcf08d2d5b6ff459
lopahn2 <lopahn2@gmail.com>
2022-06-24 10:59:09 +0000

    1

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def solution(new_id):
     while '..' in answer:
         answer = answer.replace('..', '.')
     # 4
-    if answer[0] == '.':
+    if answer[0] == '.':1
         answer = answer[1:] if len(answer) > 1 else '.'
     if answer[-1] == '.':
         answer = answer[:-1]
