8ac0f29cf34398bbd3cc6922e4128b4ec4e1f1d8
lopahn2 <lopahn2@gmail.com>
2022-07-05 14:38:06 +0000

    0

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-print('123'.ljust(5))
-print('123'.rjust(5))
-print('123'.center(5))
+from collections import defaultdict
 
-print('123'.ljust(5,'*'))
-print('123'.rjust(5,'*'))
-print('123'.center(5,'*'))
+portfolio = [
+    ('GOOG', 100, 490.1),
+    ('IBM', 50, 91.1),
+    ('CAT', 150, 83.44),
+    ('IBM', 100, 45.23),
+    ('GOOG', 75, 572.45),
+    ('AA', 50, 23.15)
+]
+
+holdings = defaultdict(list)
+
+for name, shares, price in portfolio:
+    holdings[name].append((shares, price))
+    
+print(holdings['IBM']) # [ (50, 91.1), (100, 45.23) ]
